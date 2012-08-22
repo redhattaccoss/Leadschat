c9c07111db9d24cb7dc1767cc9df33be711d0b0f
Allanaire Tapion <slasher_online@Yahoo.com>
2012-08-22 05:30:41 -0700

    Updates on Static Pages

---
+++ application/forms/Owner/RegistrationStep1.php
@@ -0,0 +1,53 @@
+<?php
+class Owner_RegistrationStep1 extends Zend_Form{
+	public function init(){
+		$this->addDecorators(array("ViewHelper"), array("Errors"));
+		$firstname = new Zend_Form_Element_Text("first_name");
+		$firstname->setLabel("Firstname");
+		$firstname->setRequired(true);
+		$firstname->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true),
+		array("validator"=>"alpha",
+				"options"=>array("allowWhiteSpace"=>false)),
+		array("validator"=>"stringLength",
+				"options"=>array(6, 50))
+			
+		));
+		$lastname = new Zend_Form_Element_Text("last_name");
+		$lastname->setLabel("Lastname");
+		$lastname->setRequired(true);
+		$lastname->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true),
+		array("validator"=>"alpha",
+				"options"=>array("allowWhiteSpace"=>false)),
+		array("validator"=>"stringLength",
+				"options"=>array(6, 50))
+			
+		));
+		$email = new Zend_Form_Element_Text("email");
+		$email->setLabel("Email Address");
+		$email->setRequired(true);
+		$email->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"emailAddress"),
+		array("validator"=>"stringLength",
+				"options"=>array(6, 50))			
+		);
+		$confirm_email = new Zend_Form_Element_Text("confirm_email");
+		$confirm_email->setLabel("Email Address");
+		$confirm_email->setRequired(true);
+		$confirm_email->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"emailAddress"),
+		array("validator"=>"stringLength",
+				"options"=>array(6, 50))			
+		);
+		
+		$this->addElements(array($firstname, $lastname, $email, $confirm_email));
+		
+	}
+}
---
+++ application/forms/Owner/RegistrationStep2.php
@@ -0,0 +1,123 @@
+<?php
+class Owner_RegistrationStep2 extends Zend_Form{
+	public function init(){
+		$this->addDecorators(array("ViewHelper"), array("Errors"));
+		$company = new Zend_Form_Element_Text("company");
+		$company->setLabel("Company");
+		$company->addValidators(array(array("validator"=>"stringLength", "options"=>array(0, 20))));
+		$items = array();
+		$items[""] = "Please Select";
+		$numberOfHitModel = new App_NumberOfHit();
+		$hits = $numberOfHitModel->fetchAll()->toArray();
+		foreach($hits as $hit){
+			$items[$hit["id"]] = $hit["name"];
+		}
+
+		$businessType = new Zend_Form_Element_Text("business_type");
+		$businessType->setRequired(true);
+		$businessType->setLabel("Business Type");
+
+		$timezoneGroupModel = new App_TimezoneGroup();
+		$timezoneGroups = $timezoneGroupModel->getAllTimezonesGrouped();
+		$items = array();
+		$items[""] = "Please Select";
+		foreach($timezoneGroups as $timezoneGroup){
+			$timezones = $timezoneGroup["timezones"];
+			foreach($timezones as $timezone){
+				$items[$timezoneGroup["name"]][$timezone["timezone_id"]]=$timezone["name"];
+			}
+		}
+		$timezone = new Zend_Form_Element_Select("timezone_id");
+		$timezone->setRequired(true);
+		$timezone->setLabel("What time zone is your business located?");
+		$timezone->addMultiOptions($items);
+		
+
+		$website = new Zend_Form_Element_Text("website");
+		$website->setRequired(true);
+		$website->setLabel("Website");
+		$website->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"alnum",
+				  "options"=>array("allowWhiteSpace"=>false)),
+		array("validator"=>"stringLength",
+				"options"=>array(13, 255))			
+		);
+				
+		$number_hits = new Zend_Form_Element_Select("number_of_hit_id");
+		$number_hits->setRequired(true);
+		$number_hits->setLabel("How many web hits do you currently receive each month?");
+		$number_hits->addMultiOptions($items);
+
+		$mobile = new Zend_Form_Element_Text("mobile");
+		$mobile->setLabel("Contact <span class=\"help\">(Skype, Mobile)</span>");
+		$mobile->setRequired(true);
+		$mobile->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"stringLength",
+				"options"=>array(10, 20))			
+		);
+		$mobile->getDecorator("Label")->setOption("escape", false);			
+		
+		$address1 = new Zend_Form_Element_Text("address1");
+		$address1->setLabel("Address 1");
+		$address1->setRequired(true);
+		$address1->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"stringLength",
+				"options"=>array(10, 255))			
+		);
+		$address2 = new Zend_Form_Element_Text("address2");
+		$address2->setLabel("Address 1");
+		$address2->setRequired(true);
+		$address2->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"stringLength",
+				"options"=>array(10, 255))			
+		);				
+		
+		$city = new Zend_Form_Element_Text("city");
+		$city->setLabel("City");
+		$city->setRequired(true);
+		$city->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"stringLength",
+				"options"=>array(10, 255))			
+		);
+		
+		$postal = new Zend_Form_Element_Text("postal");
+		$postal->setLabel("Postal Code");
+		$postal->setRequired(true);
+		$postal->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"stringLength",
+				"options"=>array(10, 30))			
+		);
+		
+		$state = new Zend_Form_Element_Text("state");
+		$state->setLabel("State");
+		$state->setRequired(true);
+		$state->addValidators(array(
+		array("validator"=>"NotEmpty",
+				  "breakChainOnFailure"=>true)),
+		array("validator"=>"stringLength",
+				"options"=>array(10, 30))			
+		);
+		
+		$items = array();
+		$items[""] = "Please Select";
+		$countryModel = new App_Country();		
+		$countries = $countryModel->fetchAll()->toArray();
+		foreach($countries as $country){
+			$items[$country["country_id"]] = $country["name"];
+		}
+
+
+	}
+}
